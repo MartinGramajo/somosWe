961f09eb5e78b133834c0021b99fa585895e7620
¨mago¨ <martingramajo08@gmail.com>
2023-09-24 13:48:44 -0300

    agrego el contenedor de financiamiento

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,9 +1,11 @@
+import Financiamiento from "../components/Financiamiento";
 import SliderHome from "../components/SliderHome";
 
 const Home = () => {
   return (
     <div>
       <SliderHome />
+      <Financiamiento />
     </div>
   );
 };
