39036de0dac58c2e2736ded5cc5faec7af20c7f7
¨mago¨ <martingramajo08@gmail.com>
2023-09-30 14:06:08 -0300

    Agrego contenido en servicios

---
+++ src/views/Servicios.jsx
@@ -0,0 +1,13 @@
+import ContenidoServicios from "../components/ContenidoServicios";
+import Wsp from "../components/Wsp";
+
+const Servicios = () => {
+  return (
+    <div className="bg-white">
+      <ContenidoServicios />
+      <Wsp />
+    </div>
+  );
+};
+
+export default Servicios;
