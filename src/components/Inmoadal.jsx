982a199a522ad3e21649813dd7fdbd9a590d6bb6
¨mago¨ <martingramajo08@gmail.com>
2023-09-30 12:29:24 -0300

    agrego el componente inmoadal

---
+++ src/components/Inmoadal.jsx
@@ -0,0 +1,12 @@
+import { Image } from "react-bootstrap";
+import inmoadal from "../assets/img/inmoadal.png";
+
+const Inmoadal = () => {
+  return (
+    <div className="d-flex justify-content-center text-white ">
+      <Image className="" src={inmoadal} alt="wsp" fluid />
+    </div>
+  );
+};
+
+export default Inmoadal;
