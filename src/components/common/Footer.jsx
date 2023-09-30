d65c292f4b5634ba967e79133491b1f8bb0127a1
¨mago¨ <martingramajo08@gmail.com>
2023-09-30 14:06:35 -0300

    modifico el footer

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,11 +1,12 @@
 import { Image } from "react-bootstrap";
+import dkz from "../../assets/img/logodkz.png";
 
 const Footer = () => {
   return (
     <div className="mt-auto bg-warning">
       <div className="text-center">
         <h6>Prod by</h6>
-        <Image alt="logo dkz" />
+        <Image className="icono-footer" src={dkz} alt="logo dkz" />
       </div>
     </div>
   );
