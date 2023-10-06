5fe728c7a7c18d7757c27b3996dd1520e94d99e4
¨mago¨ <martingramajo08@gmail.com>
2023-10-05 22:31:50 -0300

    corrijo error en contenido javier

--- src/components/ContenidoJavier.jsx
+++ src/components/ContenidoJavier.jsx
@@ -1,6 +1,6 @@
 import { Button, Image } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import javier from "../assets/img/javier.png";
+import javier from "../assets/img/Javier.png";
 
 const ContenidoJavier = () => {
   return (
