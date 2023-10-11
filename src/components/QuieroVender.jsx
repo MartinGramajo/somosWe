e7afd015a305be3a4a8072b91d2d694bcc0f2faf
¨mago¨ <martingramajo08@gmail.com>
2023-10-10 22:25:29 -0300

    quiero vender

--- src/components/QuieroVender.jsx
+++ src/components/QuieroVender.jsx
@@ -1,6 +1,7 @@
 import { Image } from "react-bootstrap";
 import { useState } from "react";
-import logo from "../assets/img/circuloLogoPrueba.png";
+import quiero from "../assets/img/quiero-vender.png";
+import { Link } from "react-router-dom";
 
 export default function QuieroVender() {
   const [navbarPosition, setNavbarPosition] = useState(false);
@@ -18,23 +19,18 @@ export default function QuieroVender() {
   return (
     <div className="d-flex justify-content-center">
       <div className="navbar-bordes">
-        <a
-          className=""
-          href="https://res.cloudinary.com/ddhhafebk/image/upload/v1675976284/media-pagina-web/reservaboton2.3c68cec5229ef200b3d9_wqtb7m_cxjb6g.png"
-          target="_blank"
-          rel="noreferrer"
-        >
+        <Link className="" to="/" target="_blank" rel="noreferrer">
           <Image
             className={
               navbarPosition
                 ? "icono-reserva fixed-top ms-auto margen-reserva me-2"
                 : "icono-reserva fixed-top ms-auto me-2 margen-reserva2"
             }
-            src={logo}
-            alt="wsp"
+            src={quiero}
+            alt="quiero vender"
             fluid
           />
-        </a>
+        </Link>
       </div>
     </div>
   );
