43b1d731b74202f4c63a4a3e4831dbfc5853e218
¨mago¨ <martingramajo08@gmail.com>
2023-10-22 13:44:37 -0300

    modifico el donde encontrarnos y el bg-pagina

--- src/components/DondeEncontrarnos.jsx
+++ src/components/DondeEncontrarnos.jsx
@@ -7,14 +7,14 @@ const DondeEncontrarnos = () => {
       <div className="text-center text-white">
         <h1>¿DÓNDE ENCONTRARNOS?</h1>
       </div>
-      <div>
+      <div className="bg-white">
         <a
-          className="text-decoration-none"
+          className="text-decoration-none d-flex justify-content-center py-5"
           target="_blank"
           rel="noreferrer"
           href="https://www.google.com/maps/place/WeReal+Grupo+Inmobiliario/@38.3416743,-0.4959143,17z/data=!4m6!3m5!1s0xd6237e52d1b22ef:0x2891174fc0f679e2!8m2!3d38.3416743!4d-0.4933394!16s%2Fg%2F11jxd905wr?entry=ttu"
         >
-          <Image className="w-100" src={donde} alt=" google maps" />
+          <Image className="donde-map" src={donde} alt=" google maps" />
         </a>
       </div>
     </div>
